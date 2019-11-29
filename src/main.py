9087949b1a5969dca17b8bf5ec82e0598395e561
Allarious <alirezaicq4@gmail.com>
2019-11-29 10:15:15 +0330

    uh really?

--- src/main.py
+++ src/main.py
@@ -5,30 +5,30 @@ from search import *
 
 
 def main():
-    token_list_persian = []
-    token_list_english = []
+    # token_list_persian = []
+    # token_list_english = []
     # persian_documents = read_xml(
     #     '../raw-database/Persian.xml', '{http://www.mediawiki.org/xml/export-0.10/}')
-    english_documents = read_csv('../raw-database/English.csv')
+    # english_documents = read_csv('../raw-database/English.csv')
     # for i in range(len(persian_documents)):
     #     token_list_persian.append(persian_preprocess(persian_documents[i]))
-    for i in range(len(english_documents)):
+    # for i in range(len(english_documents)):
         # feed in the body of documents index 1
-        token_list_english.append(english_preprocess(english_documents[i]))
+        # token_list_english.append(english_preprocess(english_documents[i]))
     #
     # # removing stopwords from term lists
-    preprocessed_persian = stopwords(token_list_persian)
-    preprocessed_english = stopwords(token_list_english)
+    # preprocessed_persian = stopwords(token_list_persian)
+    # preprocessed_english = stopwords(token_list_english)
     #
     # # creating indexing tables
-    index_table = insert_index(IndexTable([], False, True), preprocessed_persian, 1)
-    index_table = insert_index(index_table, preprocessed_english, len(preprocessed_persian))
+    # index_table = insert_index(IndexTable([], False, True), preprocessed_persian, 1)
+    # index_table = insert_index(index_table, preprocessed_english, len(preprocessed_persian))
     # #
     # # creating bigram indexing tables
-    index_table = insert_bigram_index(index_table, preprocessed_persian, 1)
-    index_table = insert_bigram_index(index_table, preprocessed_english, len(preprocessed_persian))
+    # index_table = insert_bigram_index(index_table, preprocessed_persian, 1)
+    # index_table = insert_bigram_index(index_table, preprocessed_english, len(preprocessed_persian))
     # save_to_file(index_table, "../output/index_table_gamma_persian.csv")
-    # index_table = read_from_file("../output/index_table_gamma_persian.csv", False, True)
+    index_table = read_from_file("../output/index_table_gamma_persian.csv", False, True)
 
     # get input and search the term
     # while True:
